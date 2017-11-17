e33e54516bcdc03af482b10afd8971ff908a989f
Kevin Cole <dc.loco@gmail.com>
2017-11-17 08:18:18 -0500

    - Initial commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Seminal Works
+
+Herein layeth code developed in the service of creating a FLOSS / FLOSH
+Scanning Electron Microscope (SEM).
+
+## _Horror vacui metior_ (Nature Abhors a Vacuum Gauge)
+
+First up: **monitor.py**
+
+This little ditty logs the pressure read from the [MKS Instruments 925
+Micro Pirani(TM) Vacuum
+Transducer](https://www.mksinst.com/product/Product.aspx?ProductID=648)
+ten times per second, both to the screen and to a file named
+**vacuum.log**.
+
+Press **`Enter`** to begin logging. Press **`Ctrl-C`** to close the log
+and exit.
+
+For specs on the vacuum transducer, see:
+https://www.mksinst.com/product/Product.aspx?ProductID=648
+
+## Copyright and License
+
+Copyright 2017 Kevin Cole <kevin.cole@novawebcoop.org> 2017.11.17
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
---
+++ monitor.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  monitor.py
+#  Copyright 2017 Kevin Cole <kevin.cole@novawebcoop.org> 2017.11.17
+#
+#  Nature Abhors a Vacuum Gauge
+#  (horror vacui metior)
+#
+#  This little ditty logs the pressure read from the [MKS Instruments 925 Micro
+#  Pirani(TM) Vacuum Transducer] ten times per second, both to the screen and
+#  to a file named vacuum.log.
+#
+#    * Press [Enter] to begin logging.
+#    * Press Ctrl-C  to close the log and exit.
+#
+#  For specs on the vacuum transducer, see:
+#  https://www.mksinst.com/product/Product.aspx?ProductID=648
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public
+#  License along with this program; if not, write to the Free
+#  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301, USA.
+#
+
+
+import os
+import sys
+import serial
+import re
+from   time   import sleep, strftime
+
+
+__appname__    = "horror vacui metior"
+__author__     = "Kevin Cole"
+__copyright__  = "Copyright \N{copyright sign} 2017"
+__agency__     = "HacDC"
+__credits__    = ["Kevin Cole"]  # Authors and bug reporters
+__license__    = "GPL"
+__version__    = "1.0"
+__maintainer__ = "Kevin Cole"
+__email__      = "kjcole@member.fsf.org"
+__status__     = "Prototype"  # "Prototype", "Development" or "Production"
+__module__     = ""
+
+
+def main():
+    _ = os.system("clear")
+    print("{0} v.{1}\n{2} ({3})\n{4}, {5} <{6}>\n"
+          .format(__appname__,
+                  __version__,
+                  __copyright__,
+                  __license__,
+                  __author__,
+                  __agency__,
+                  __email__))
+
+    input("Nature abhors a vacuum gauge. Press [Enter] to start, Ctrl-C to end.")
+    
+    mks925 = serial.Serial("/dev/ttyS0", 9600, timeout=0, rtscts=False)
+    query = {}
+    query["pressure-3"] = b"@254PR1?;FF"  # Return pressure as a 3-digit value
+    query["pressure-4"] = b"@254PR4?;FF"  # Return pressure as a 4-digit value
+
+    ack = {}
+    ack["pressure"] = re.compile(b"@\d{3}ACK(.*);FF")
+
+    nak = {}
+    nak["syntax"]   = re.compile(b"@\d{3}NAK160;FF")
+
+    log = open("vacuum.log", "w")
+
+    entry = "Logging begun at: {0}\n".format(strftime("%Y-%m-%d %H:%M:%S"))
+    sys.stdout.write(entry)
+    log.write(entry)
+
+    while True:
+        timestamp = strftime("%Y-%m-%d %H:%M:%S")
+        try:
+            mks925.write(query["pressure-4"])
+            response = mks925.read(100)
+            found = ack["pressure"].match(response)
+            if found:
+                torr      = float(found.groups()[0])
+                entry = "{0} {1}\n".format(timestamp, torr)
+                sys.stdout.write(entry)
+                log.write(entry)
+            sleep(0.1)
+        except KeyboardInterrupt:
+            entry = "Logging finished at: {0}\n".format(timestamp)
+            sys.stdout.write(entry)
+            log.write(entry)
+            log.close()
+            sys.exit()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
